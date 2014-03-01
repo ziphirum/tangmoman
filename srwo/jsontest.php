41dd876715be2200f7266a760fb8d4b29c874944
kniezz <kniezz@hotmail.com>
2014-03-01 18:43:46 +0700

    json test 2 fix

--- srwo/jsontest.php
+++ srwo/jsontest.php
@@ -1,5 +1,5 @@
 <?php
-	$conn = mysqli_connect("localhost","root","password","tangmo","3306");
+	$conn = mysqli_connect("localhost","root","password","","3306");
 	
 	if (mysqli_connect_errno($conn)){	
 		echo "Failed to connect to MySQL: " . mysqli_connect_error();
