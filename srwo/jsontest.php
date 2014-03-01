7591e5af1a78de3eea8a1d288e1526a87654e5b2
kniezz <kniezz@hotmail.com>
2014-03-01 19:51:19 +0700

    dbconnection

--- srwo/jsontest.php
+++ srwo/jsontest.php
@@ -1,9 +1,10 @@
 <?php
-	$conn = mysqli_connect("localhost","root","password","","3306");
+	$conn = mysqli_connect("localhost","root","password","tangmoman","3306");
 	
 	if (mysqli_connect_errno($conn)){	
 		echo "Failed to connect to MySQL: " . mysqli_connect_error();
 	}else{
 		echo "success";
-	}	
+	}
+	
 ?>
