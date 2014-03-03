074d662e7a782681256f10ea9b4adda8d6fbe535
kniezz <kniezz@hotmail.com>
2014-03-03 15:52:30 +0700

    common

--- srwo/common.php
+++ srwo/common.php
@@ -4,17 +4,10 @@
 		return "'".$str."'" ;
 	}
 
-<<<<<<< HEAD
 	function classToJson($obj){
 		$obj_vars = $obj.getObjectVars();
 
 		foreach ($obj_vars as $name => $value) {
-=======
-	function classToJson($class){
-		$class_vars = get_class_vars(get_class($class));
-		echo 'Class:<br>';
-		foreach ($class_vars as $name => $value) {
->>>>>>> FETCH_HEAD
 		    echo "$name : $value\n";
 		}
 	}
