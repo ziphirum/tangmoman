1a621baae9a8652a2199ed699a41d8505052d60d
kniezz <kniezz@hotmail.com>
2014-03-03 23:16:42 +0700

    sd

--- srwo/common.php
+++ srwo/common.php
@@ -9,7 +9,8 @@
 		$args = func_get_args();
 		foreach ($args as $obj){
 			$obj_vars = $obj->getObjectVars();
-			$arr_obj_vars[get_class($obj)] = $obj_vars;
+			$obj_vars[getclass] = get_class($obj)
+			$arr_obj_vars[] = $obj_vars;
 		}
 		return json_encode($arr_obj_vars);
 	}
