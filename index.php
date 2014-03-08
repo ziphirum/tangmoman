2f7ca964a2edec34f5d618c43bc47cbaee608126
Petzh <ziphirum@gmail.com>
2014-03-08 22:59:14 +0700

    escape string test

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
 <?php
     $arr = array ('a'=>1,'b'=>2,'c'=>3,'d'=>4,'e'=>5);
-    echo json_encode($arr); // {"a":1,"b":2,"c":3,"d":4,"e":5}
+    // echo json_encode($arr); // {"a":1,"b":2,"c":3,"d":4,"e":5}
+
+    $item = "Zak;\"'s Laptop;";
+	$escaped_item = mysql_escape_string($item);
+	printf("Escaped string: %s\n", $escaped_item);
+
 ?>
