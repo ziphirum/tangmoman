ecae5aac55c92aa11a04115c2d7f5c09d89a4580
Petzh <ziphirum@gmail.com>
2014-03-12 22:44:22 +0700

    update fight

--- srwo/fight.php
+++ srwo/fight.php
@@ -1,6 +1,7 @@
 <?php
 	
-	include 'constants.php';
+	include 'database.php';
+	include 'class.php';
 
 	function defenderList($userid){
 		$conn = openConn();
@@ -34,14 +35,6 @@
 		return json_encode($arr_obj_vars);
 	}
 	
-	function classToJsonX(){
-		$arr_obj_vars = array();
-		$args = func_get_args();
-		foreach ($args as $obj){
-			$obj_vars = $obj->getObjectVars();
-			$obj_vars[getClass] = get_class($obj);
-			$arr_obj_vars[] = $obj_vars;
-		}
-		return json_encode($arr_obj_vars);
-	}
+	$response = defenderList(1);
+	echo $response;
 ?>
