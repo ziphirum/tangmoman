4ce3e8eed9bf58f8ad7a172970d45bf5ad1b1cf7
kniezz <kniezz@hotmail.com>
2014-04-06 17:40:32 +0700

    json fix

--- srwo/stat.php
+++ srwo/stat.php
@@ -1,23 +1,29 @@
 <?php
 
-	include 'database.php';
-	include 'common.php';
-	include 'class.php';
-
-	$username = $_GET['username'];
+	include "database.php";
+	include "common.php";
+	include "class.php";
+	include "session.php";
+	
+	$userid = getLoginSession();
 	$conn = openConn();
-	$sql = "SELECT *  FROM tm_useraccount WHERE username= ".sqlStr($username);
+	$sql = "SELECT *  FROM tm_useraccount WHERE id= ".sqlStr($userid);
 
 	$result = mysqli_query($conn, $sql);
-	$userId;
+	$userId = "";
 	while($row = mysqli_fetch_array($result)){
 		$userId = $row['id'];
 	}
 	closeConn($conn);
-
-	$user = new UserAccount($userId);
-	$char = new Character($userId);
-
-	echo classToJson($user, $char);
+	
+	if($userId == ""){
+		$result = array ("status" => "ERROR");
+		echo json_encode($result);
+	}else{
+		$user = new UserAccount($userId);
+		$char = new Character($userId);
+		echo classToJson($user, $char);
+	}
+	
 
 ?>
