58a59c2e142ff887a93654131a291ffbd529ea6e
Petzh <ziphirum@gmail.com>
2014-04-06 18:56:02 +0700

    fight

--- srwo/fight.php
+++ srwo/fight.php
@@ -10,10 +10,22 @@
 
 	function fight($attacker, $defender){
 		// attacker -1 lose , 0 draw, 1 win
-		$resultMap = array(-1 => "LOST", 0 => "DRAW", 1 => "WIN");
-		$result = $resultArr[rand(-1,1)];
+		// $resultMap = array(-1 => "LOST", 0 => "DRAW", 1 => "WIN");
+		// $result = $resultArr[rand(-1,1)];
 
-		echo $result;
+		// echo $result;
+
+		$conn = openConn();
+		// Check connection
+
+		mysqli_query($conn,"INSERT INTO Persons (FirstName, LastName, Age)
+		VALUES ('Peter', 'Griffin',35)");
+
+		mysqli_query($conn,"INSERT INTO Persons (FirstName, LastName, Age) 
+		VALUES ('Glenn', 'Quagmire',33)");
+
+		mysqli_close($conn);
+
 	}
 	
 
