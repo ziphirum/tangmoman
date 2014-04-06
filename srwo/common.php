b1ffc25a6005b4f3573ce2bb02e5bb912b4ae202
kniezz <kniezz@hotmail.com>
2014-04-06 17:33:15 +0700

    json fix

--- srwo/common.php
+++ srwo/common.php
@@ -11,8 +11,8 @@
 			$obj_vars = $obj->getObjectVars();
 			//$obj_vars[getClass] = get_class($obj);
 			$arr_obj_vars[get_class($obj)] = $obj_vars;
-			$arr_obj_vars[Status] = "OK";
 		}
+		$arr_obj_vars[Status] = "OK";
 		return json_encode($arr_obj_vars);
 	}
 ?>
