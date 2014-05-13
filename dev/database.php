f375dcf43ae847a6a15757c7d47eb16b8d42338e
kniezz <kniezz@hotmail.com>
2014-05-13 22:24:06 +0700

    bn

--- dev/database.php
+++ dev/database.php
@@ -27,7 +27,7 @@
 		$att = "";
 		$sqlval = "";
 		$aval = array();
-		$aparm = array();
+		$aparam = array();
 		foreach ($insertArray as $arr){
 			$col .= $arr[0].",";
 			$aval[] = $arr[1];
@@ -37,14 +37,17 @@
 		
 		$col = substr($col,0,-1);
 		$sqlval = substr($sqlval,0,-1);
-		
-		$aparm = array_merge(array($att),$aval);
+		$aparam[] = &$att;
+ 
+		for($i=0;$i<count($aval);$i++) {
+		  $aparam[] = &$aval[$i];
+		}
 		
 		$sql = "INSERT INTO ".$table." (".$col.") VALUES(".$sqlval.")";
 		
 		$conn = openConn();
 		$stmt = $conn->prepare($sql);
-		call_user_func_array(array(&$stmt,"bind_param"), refValues($aparm));
+		call_user_func_array(array(&$stmt,"bind_param"), $aparam);
 		if($stmt->execute()){
 			$retval = TRUE;
 		}
