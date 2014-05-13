aa25355b0fe4335964e1b24327ee031e6f380e82
kniezz <kniezz@hotmail.com>
2014-05-13 22:03:01 +0700

    insertsql

--- srwo/database.php
+++ srwo/database.php
@@ -19,39 +19,6 @@
 		}
 		return $rs;
 	}
-	
-	function insertSQL($table,$insertArray){
-		$retval = FALSE;
-		$sql = "";		
-		$col = "";
-		$att = "";
-		$sqlval = "";
-		$aval = array();
-		$aparm = array();
-		foreach ($insertArray as $arr){
-			$col .= $arr[0].",";
-			$aval[] = $arr[1];
-			$att .= $arr[2];
-			$sqlval .= "?,";
-		}
-		
-		$col = substr($col,0,-1);
-		$sqlval = substr($sqlval,0,-1);
-		
-		$aparm = array_merge(array($att),$aval);
-		
-		$sql = "INSERT INTO ".$table." (".$col.") VALUES(".$sqlval.")";
-		
-		$conn = openConn();
-		$stmt = $conn->prepare($sql);
-		call_user_func_array(array($stmt,"bind_param"), $aparm);
-		if($stmt->execute()){
-			$retval = TRUE;
-		}
-		closeConn($conn);
-		return $retval;
-		
-	}
 
 
 ?>
