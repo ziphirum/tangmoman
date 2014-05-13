e3c36fe8eb6d21db047946b3a52ac4e781b58bdd
kniezz <kniezz@hotmail.com>
2014-05-13 22:25:11 +0700

    fixed insertSQL

--- dev/common.php
+++ dev/common.php
@@ -46,15 +46,5 @@
 		return $randNum<$n;
 	}
 	
-	function refValues($arr){
-    if (strnatcmp(phpversion(),'5.3') >= 0) //Reference is required for PHP 5.3+
-    {
-        $refs = array();
-        foreach($arr as $key => $value)
-            $refs[$key] = &$arr[$key];
-        return $refs;
-    }
-    return $arr;
-}
 
 ?>
